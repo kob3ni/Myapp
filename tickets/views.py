b4557c8a83ba874f63d54888c48315d2ad13fc09
Natalia Petrakova <natysya030704@gmail.com>
2024-12-15 20:46:14 +0300

    Добавили фильтры на страницу с билетами и исправили пагинацию

--- tickets/views.py
+++ tickets/views.py
@@ -5,15 +5,21 @@ from tickets.models import Airports, Bookings, Flights, Seats, Ticket_flights, T
 
 def flight(request):
     page = request.GET.get('page', 1)
+    order_by = request.GET.get('order_by', None)
     
     flights = Flights.objects.all()
+    bookings = Bookings.objects.all()
     
-    paginator = Paginator(flights, 1)
+    if order_by and order_by != "default":
+        bookings = bookings.order_by(order_by)
+    
+    paginator = Paginator(flights, 3)
     current_page = paginator.page(int(page))
     
     context= {
         "title": "Avia - Билеты",
         "flights": current_page,
+        "bookings": bookings,
     }
     return render(request, 'tickets/flight.html', context)
     
